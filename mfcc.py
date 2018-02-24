014b1d85027d1ea476a183e8429540e2a38277a0
mlkwok <mkwok@uw.edu>
2018-02-24 07:10:21 -0800

    compute deltas and concatenate matrices

--- mfcc.py
+++ mfcc.py
@@ -8,20 +8,23 @@ import wave
 import math
 import scipy.fftpack
 
-SKIP        = 160                      # Numbers of samples to skip to start new frame
-SAMPLES     = 400                      # Number of samples per frame
-LOWER_FREQ  = 300.0                    # Lower frequency in Hz
-UPPER_FREQ  = 8000.0                   # Upper frequency in Hz
-FFT_LENGTH  = 512                      # Length of FFT
-FFT_KEEP    = FFT_LENGTH // 2 + 1      # Number of FFT coefficients to keep
-NFILTERS    = 26                       # Number of filterbanks for
-Fs          = 16000                    # Sampling frequency in Hz
-FRAME_LEN   = 0.025                    # Length of each frame in seconds
-AUDIO_LEN   = 4                        # Length of audio sample in seconds
+SKIP          = 160                      # Numbers of samples to skip to start new frame
+SAMPLES       = 400                      # Number of samples per frame
+LOWER_FREQ    = 300.0                    # Lower frequency in Hz
+UPPER_FREQ    = 8000.0                   # Upper frequency in Hz
+FFT_LENGTH    = 512                      # Length of FFT
+FFT_KEEP      = FFT_LENGTH // 2 + 1      # Number of FFT coefficients to keep
+NFILTERS      = 26                       # Number of filterbanks for
+Fs            = 16000                    # Sampling frequency in Hz
+FRAME_LEN     = 0.025                    # Length of each frame in seconds
+AUDIO_LEN     = 4                        # Length of audio sample in seconds
+M             = 2                        # Window size of delta features
+NCOEFFICIENTS = 13                       # Number of MFCC coefficients to keep
 
 # Import data
 raw = genfromtxt("s-data.csv", delimiter = ",")
 
+# NOTE: ROWS is actually columns. Need to fix.
 # Perform the DFT and create the power spectrum
 ROWS = (Fs * AUDIO_LEN - SAMPLES) // SKIP + 1
 power_spec = np.zeros([ROWS, FFT_LENGTH])
@@ -88,13 +91,37 @@ ifft_log_energies = scipy.fftpack.dct(log_energies)
 np.savetxt("ifft_log_energies.csv", ifft_log_energies, delimiter = ",")
 
 # Trim to keep only the first 13 energies
-mfcc = ifft_log_energies[:, 0:13]
+# Save the matrix in a 13 x T matrix
+mfcc = np.transpose(ifft_log_energies[:, 0:NCOEFFICIENTS])
 np.savetxt("mfcc.csv", mfcc, delimiter = ",")
 
 # Find deltas
-# for i in range 
+deltas = np.zeros([NCOEFFICIENTS, ROWS - 2 * M])
+for i in range(0, NCOEFFICIENTS):
+    # For each row in the MFCC matrix
+    for j in range(0, ROWS - 2 * M):
+        # Calculate summation to find delta coefficient
+        d = 0
+        for n in range(1, M + 1):
+            d = d + ((n * (mfcc[i, j + 2 * n] - mfcc[i, j])) / (2 * (n ** 2)))
+        deltas[i, j] = d
+np.savetxt("deltas.csv", deltas, delimiter = ",")
 
-# # Create spectrograms
-# img = plt.imshow(mfcc, interpolation='none', extent=[LOWER_FREQ, UPPER_FREQ, 0, ROWS], aspect = "auto")
-# plt.colorbar(img, orientation = "horizontal")
-# plt.show()
+# Trim the original MFCC matrix
+cut_mfcc = np.zeros([NCOEFFICIENTS, ROWS - 2 * M])
+for i in range(0, NCOEFFICIENTS):
+    cut_mfcc[i, :] = mfcc[i, 0:ROWS - 2 * M]
+np.savetxt("cut_mfcc.csv", cut_mfcc, delimiter = ",")
+
+# Concatenate the original MFCC matrix with the delta matrix
+# The MFCC matrix is above the delta matrix
+final = np.zeros([NCOEFFICIENTS * 2, ROWS - 2 * M])
+for i in range(0, NCOEFFICIENTS):
+    final[i, :] = cut_mfcc[i, :]
+    final[i + NCOEFFICIENTS, :] = deltas[i, :]
+np.savetxt("final.csv", final, delimiter = ",")
+
+# Create spectrograms
+img = plt.imshow(final, interpolation='none', aspect = "auto")
+plt.colorbar(img, orientation = "horizontal")
+plt.show()
